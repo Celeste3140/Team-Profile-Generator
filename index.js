2e31b85eb20fa3830b478de6135733f190a21b2f
carambola <celestebaltazar@yahoo.com>
2022-07-27 19:01:10 -0700

    add the questions

--- index.js
+++ index.js
@@ -7,3 +7,40 @@ const inquirer = require('inquirer');
 
 const employees = [];
 
+function MemberQs() {
+    inquirer.prompt({
+        message: "Team member's name?",
+        name: "name"
+    },
+    {
+       type: 'list',
+       message: "Team member's role?",
+       name: 'role',
+       choices: [
+        'Engineer',
+        'Intern',
+        'Manager'
+       ],
+    },
+    {
+        message: "Enter team member's id.",
+        name: 'id'
+    },
+    {
+        message: "Team member's Github?",
+        name: 'Github'
+    },
+    {
+        message: "Team member's email?",
+        name: 'email'
+    },
+    {
+        type: 'list',
+        message: "Add more team members?",
+        choices: [
+            'yes',
+            'no'
+        ],
+        name: 'addMembers'
+    })
+}
