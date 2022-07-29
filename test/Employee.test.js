f1222f129ca33abe48947f013c8e5086908ab8b5
carambola <celestebaltazar@yahoo.com>
2022-07-28 22:44:14 -0700

    added to test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,7 @@
 const Employee = require('../lib/employee.class')
+
+const celeste = new Employee('celeste', '20', 'cbaltazar464@gmail.com')
+
+describe('testing employee values',() => {
+    it('check name', () => {expect(name)})
+})
