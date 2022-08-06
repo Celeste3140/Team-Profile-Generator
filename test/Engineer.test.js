84ddc70ae1ba3d7cf4fea39a53c2173033255bc9
carambola <celestebaltazar@yahoo.com>
2022-08-05 23:07:34 -0700

    updated .test and lib/class folder name

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,1 +1,12 @@
 const Engineer = require('../lib/engineer.class')
+
+describe('Engineer', () => {
+    describe( 'Engineer',() => {
+        it('should return the name of Engineer', () => {
+            const engineer = NewEngineer ('Tom', 24, 'Tomnook@gmail.com');
+            expect(engineer.name.toEqual('Tom'));
+            expect(engineer.idNumber.toEqual(24));
+            expect(engineer.email.toEqual('Tomnook@gmail.com'))
+        })
+    })
+})
